bd0019ca44c71a476524bee34ea1668bfe952d04
JensvanBrouwershaven <112871892+JensvanBrouwershaven@users.noreply.github.com>
2023-06-02 08:55:34 +0200

    ...

--- add-country01.php
+++ add-country01.php
@@ -1,1 +1,53 @@
-<?php include "underconstruct.php"; ?>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="company.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <header>
+    <?php
+
+    session_start();
+    if (!isset($_SESSION["signedInAdmin"])) {
+      header_remove();
+      header("Location: index.php ");
+      exit();
+    }
+    require_once("dbconnect.php");
+    include "nav.php";
+    ?>
+  </header>
+  <form method="post" action="add-product01-script.php">
+    <h3>Voeg product toe</h3>
+    <div>
+      <label for="product">Naam Land:</label>
+      <input type="text" id="product" name="Country-name" required />
+    </div>
+    <div>
+      <label for="product">LandenCode:</label>
+      <input type="text" id="product" name="Country-Code" required />
+        <?php
+        // defines a Query for catorgory's
+        $retreiveCategories = $db->prepare("SELECT `name` FROM country");
+        $retreiveCategories->execute();
+        $categoryNames = $retreiveCategories->fetchAll();
+        // for each catorgory it will echo it in the select
+
+        ?>
+      </select>
+    </div>
+    <div>
+      </select>
+    </div>
+    <input type="submit" name="submit" id="submit" value="voeg toe">
+</body>
+
+</html>
+</body>
+
+</html>
